7f21575bf4626617ea4193e2b0464cffe4bce312
Amylot <36183787+Amylot@users.noreply.github.com>
2026-06-04 20:56:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,4 @@ def webhook():
 def home():
     return "Webhook server is running!", 200
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
